d43a9321065befa58a81a00bb4493ba27ca88544
battlejoose <battlejoose@gmail.com>
2025-08-11 01:57:45 -0700

    trying to clamp correctly and increase distance

--- public/Build/public.framework.js
+++ public/Build/public.framework.js
@@ -1274,10 +1274,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7348816: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7348871: ($0) => { performance.now = function() { return $0; }; },  
- 7348919: ($0) => { performance.now = function() { return $0; }; },  
- 7348967: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7349024: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7349079: ($0) => { performance.now = function() { return $0; }; },  
+ 7349127: ($0) => { performance.now = function() { return $0; }; },  
+ 7349175: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
