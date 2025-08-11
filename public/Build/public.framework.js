062858c30ac80f5f646e02e5caec085b243e9543
battlejoose <battlejoose@gmail.com>
2025-08-11 04:49:18 -0700

    retrying lowering it a bit

--- public/Build/public.framework.js
+++ public/Build/public.framework.js
@@ -1274,10 +1274,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7349040: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7349095: ($0) => { performance.now = function() { return $0; }; },  
- 7349143: ($0) => { performance.now = function() { return $0; }; },  
- 7349191: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7349024: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7349079: ($0) => { performance.now = function() { return $0; }; },  
+ 7349127: ($0) => { performance.now = function() { return $0; }; },  
+ 7349175: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
