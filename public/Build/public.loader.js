f8796c3d7c74f9e613c2739c8f72d1d27cab832c
battlejoose <battlejoose@gmail.com>
2025-08-10 23:23:40 -0700

    more logs

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 91861065,
+    wasmFileSize: 91861443,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
