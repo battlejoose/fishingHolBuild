aa27110b493a477534d1663835d99c3b830eb7b1
battlejoose <battlejoose@gmail.com>
2025-08-11 06:37:32 -0700

    spinner once more

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 91871866,
+    wasmFileSize: 91871983,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
