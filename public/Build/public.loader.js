b4f473d14b418efa1841c56a121dc8b45c753123
battlejoose <battlejoose@gmail.com>
2025-08-11 00:52:26 -0700

    float cam again

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 91867435,
+    wasmFileSize: 91867786,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
