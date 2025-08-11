9dd956c12a3749baabdaa3753145f7da95763223
battlejoose <battlejoose@gmail.com>
2025-08-11 03:05:40 -0700

    trying first zoom again

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 91874857,
+    wasmFileSize: 91876440,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
