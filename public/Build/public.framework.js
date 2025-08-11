88f7956e445bc63806219c3754c2444dc6c52978
battlejoose <battlejoose@gmail.com>
2025-08-11 14:30:31 -0700

    spinner maybe?

--- public/Build/public.framework.js
+++ public/Build/public.framework.js
@@ -1274,10 +1274,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7349040: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7349095: ($0) => { performance.now = function() { return $0; }; },  
- 7349143: ($0) => { performance.now = function() { return $0; }; },  
- 7349191: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7349072: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7349127: ($0) => { performance.now = function() { return $0; }; },  
+ 7349175: ($0) => { performance.now = function() { return $0; }; },  
+ 7349223: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
