47a2c1be73ae71c7d7c1de6d046a3e60d753f639
battlejoose <battlejoose@gmail.com>
2025-08-13 04:47:49 -0700

    changed size and local

--- public/Build/public.framework.js
+++ public/Build/public.framework.js
@@ -1274,10 +1274,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7350304: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7350359: ($0) => { performance.now = function() { return $0; }; },  
- 7350407: ($0) => { performance.now = function() { return $0; }; },  
- 7350455: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7350368: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7350423: ($0) => { performance.now = function() { return $0; }; },  
+ 7350471: ($0) => { performance.now = function() { return $0; }; },  
+ 7350519: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 91917416,
+    wasmFileSize: 91918315,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
