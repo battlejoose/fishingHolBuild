3d7589c3b49ebf13155b00a5ff43de6038d1fb3e
battlejoose <battlejoose@gmail.com>
2026-01-09 05:15:39 -0700

    same spot cast fix

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 92425556,
+    wasmFileSize: 92425453,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
