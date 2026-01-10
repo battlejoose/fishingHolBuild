5dc5e4cdaa0075e88fc3c95814eb006af6bffac3
battlejoose <battlejoose@gmail.com>
2026-01-10 07:55:00 -0700

    connect wallet button

--- public/Build/public.framework.js
+++ public/Build/public.framework.js
@@ -1275,10 +1275,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  9291984: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 9292039: ($0) => { performance.now = function() { return $0; }; },  
- 9292087: ($0) => { performance.now = function() { return $0; }; },  
- 9292135: () => { performance.now = Module['emscripten_get_now_backup']; }
+  9292224: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 9292279: ($0) => { performance.now = function() { return $0; }; },  
+ 9292327: ($0) => { performance.now = function() { return $0; }; },  
+ 9292375: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 118946855,
+    wasmFileSize: 118950845,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
