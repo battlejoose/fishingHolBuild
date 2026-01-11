5cb24bb68341f6d0f6de8ffaef26b4cf0bf16951
battlejoose <battlejoose@gmail.com>
2026-01-10 17:25:06 -0700

    require tokens for bait

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 118954684,
+    wasmFileSize: 118964152,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
