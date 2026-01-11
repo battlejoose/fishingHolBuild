0561ae13a12f1fddc10bb5dba4aa00370d50df64
battlejoose <battlejoose@gmail.com>
2026-01-11 11:27:30 -0700

    ui changes cleaning

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 118965422,
+    wasmFileSize: 119000374,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
