c0c1852ae942a3787e077608aaf9c399696c2b66
battlejoose <battlejoose@gmail.com>
2026-01-10 20:32:44 -0700

    trying real equip

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 118964354,
+    wasmFileSize: 118964167,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
