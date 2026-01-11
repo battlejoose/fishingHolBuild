6edc6586bda66f25616f2d044b2db62acf4d6df0
battlejoose <battlejoose@gmail.com>
2026-01-10 20:12:33 -0700

    holding rods on spawn

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 118964152,
+    wasmFileSize: 118964354,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
