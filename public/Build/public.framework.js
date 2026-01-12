2e53b51ad8713a7cd0bf7b5bab57cd41821e4805
battlejoose <battlejoose@gmail.com>
2026-01-11 20:28:46 -0700

    spawning with a worm

--- public/Build/public.framework.js
+++ public/Build/public.framework.js
@@ -1275,10 +1275,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  9295360: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 9295415: ($0) => { performance.now = function() { return $0; }; },  
- 9295463: ($0) => { performance.now = function() { return $0; }; },  
- 9295511: () => { performance.now = Module['emscripten_get_now_backup']; }
+  9295376: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 9295431: ($0) => { performance.now = function() { return $0; }; },  
+ 9295479: ($0) => { performance.now = function() { return $0; }; },  
+ 9295527: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 119002999,
+    wasmFileSize: 119003131,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
