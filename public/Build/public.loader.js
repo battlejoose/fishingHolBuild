76221dd3627f369ee038f61c162ac598684e0d9d
battlejoose <battlejoose@gmail.com>
2026-02-05 13:12:54 -0700

    sweet tap dancing rollback

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 118996849,
+    wasmFileSize: 118998126,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
