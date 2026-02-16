c546171a5fabefff287de4264d0fd9a37b9686f3
battlejoose <battlejoose@gmail.com>
2026-02-16 10:15:55 -0700

    trying double mobile ui size for backback

--- public/Build/public.framework.js
+++ public/Build/public.framework.js
@@ -1275,10 +1275,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  9298704: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 9298759: ($0) => { performance.now = function() { return $0; }; },  
- 9298807: ($0) => { performance.now = function() { return $0; }; },  
- 9298855: () => { performance.now = Module['emscripten_get_now_backup']; }
+  9298736: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 9298791: ($0) => { performance.now = function() { return $0; }; },  
+ 9298839: ($0) => { performance.now = function() { return $0; }; },  
+ 9298887: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
@@ -1868,6 +1868,10 @@ var ASM_CONSTS = {
         ((a1) => dynCall_vi.apply(null, [callback, a1]))(isXnft);
       }
     }
+
+  function _IsMobileBrowser() {
+       return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+     }
 
   var JS_Accelerometer = null;
   
@@ -18871,6 +18875,7 @@ var wasmImports = {
   "GetJSLoadTimeInfo": _GetJSLoadTimeInfo,
   "GetJSMemoryInfo": _GetJSMemoryInfo,
   "InitWalletAdapter": _InitWalletAdapter,
+  "IsMobileBrowser": _IsMobileBrowser,
   "JS_Accelerometer_IsRunning": _JS_Accelerometer_IsRunning,
   "JS_Accelerometer_Start": _JS_Accelerometer_Start,
   "JS_Accelerometer_Stop": _JS_Accelerometer_Stop,
--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 119046327,
+    wasmFileSize: 119046789,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
