7466ca368d1723eb8335ae1e10079dd02b7f0839
battlejoose <battlejoose@gmail.com>
2026-02-17 17:15:17 -0700

    updated ca

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 119018952,
+    wasmFileSize: 119018804,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
