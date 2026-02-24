ba29325edaa11b5e878ba14922adba25bdbeb915
battlejoose <battlejoose@gmail.com>
2026-02-24 09:37:39 -0700

    backpack disabled during fishing

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 130289858,
+    wasmFileSize: 130290292,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
