524d1a702dbb95b18ade22441d528e3f713a803b
battlejoose <battlejoose@gmail.com>
2026-02-23 17:28:00 -0700

    lose spinners too

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 130289646,
+    wasmFileSize: 130289718,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
