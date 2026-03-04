5b2670b74b4e050207f54842e6eedb4aaba1fa0b
battlejoose <battlejoose@gmail.com>
2026-03-04 11:18:39 -0700

    auto cast checks for land

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 129655515,
+    wasmFileSize: 129657957,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
