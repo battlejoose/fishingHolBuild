b6d44c3cf9d15004930688db2ea09ff806d433e4
battlejoose <battlejoose@gmail.com>
2026-03-07 00:08:16 -0700

    trying button again

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 130306390,
+    wasmFileSize: 130306564,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
