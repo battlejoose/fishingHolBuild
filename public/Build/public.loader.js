b792d17e298df792a280d464ce96917444412ad3
battlejoose <battlejoose@gmail.com>
2026-03-06 18:52:52 -0700

    marlin

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 129666231,
+    wasmFileSize: 130306565,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
