4ac9215cdeebeef08dd2f21d96abf814e88de5d4
battlejoose <battlejoose@gmail.com>
2026-03-06 21:16:48 -0700

    fish showing

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 130306341,
+    wasmFileSize: 130306492,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
